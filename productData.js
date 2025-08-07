07772535e534062de88d99f2f415f9d6082ed356
305699 <mrajrich01@gmail.com>
2025-08-07 13:22:41 +0530

    productData.js

---
+++ productData.js
@@ -0,0 +1,12 @@
+// Dummy Products
+module.exports = {
+  "P123": {
+    name: "Premium Oil",
+    price: 20
+  },
+  "P456": {
+    name: "Ayurvedic Soap",
+    price: 20
+  },
+  // Add more product codes
+};
